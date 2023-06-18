69a8636ae9be4d8f79bbbed2775c5855ebf3384e
TungNguyen0510 <88915666+TungNguyen0510@users.noreply.github.com>
2023-06-18 13:43:05 +0700

    add recruit

---
+++ src/feature/recruit/index.js
@@ -0,0 +1,117 @@
+import BoxBody from "../../components/box-body/index";
+import {
+    Box,
+    Typography,
+} from "@mui/material";
+export default function Recruit() {
+    return (
+        <BoxBody>
+            <Box textAlign="justify">
+                <Typography
+                    variant="h1"
+                    color="#555"
+                    fontSize="22px"
+                    fontWeight="550"
+                    mb={2}
+                >
+                    Mô tả công việc
+                </Typography>
+                <Typography
+                    variant="h2"
+                    color="#777"
+                    fontSize="16px"
+                    fontWeight="550"
+                    mb={2}
+                >
+                    Cửa hàng Khung & Tranh của chúng tôi đang cần tuyển 2 họa sĩ vẽ tranh sơn dầu
+                </Typography>
+                <Typography
+                    variant="subtitle"
+                    color="#777"
+                    fontSize="14px"
+                    
+                >
+                    – Có thể vẽ được tất cả các thể loại tranh bao gồm như: Tranh phong cảnh, tĩnh vật,
+                    trừu tượng, hiện đại, đặc biệt nếu vẽ được chân dung là một lợi thế.<br />
+                    – Ưu tiên những bạn đã có kinh nghiệm, đã từng làm việc và vẽ tranh cho các xưởng vẽ tranh.<br />
+                    – Cụ thể trao đổi khi phỏng vấn.
+                </Typography>
+                <Typography
+                     variant="h2"
+                     color="#777"
+                     fontSize="16px"
+                     fontWeight="550"
+                     mt={2}
+                >
+                    Yêu cầu công việc
+                </Typography>
+                <Typography
+                     variant="subtitle"
+                     color="#777"
+                     fontSize="14px"
+                     
+                >
+                    – Tư duy màu và bố cục tốt, có khả năng sáng tác. <br />
+                    – Trung thực, chỉn chu và sáng tạo.<br />
+                    – Ưu tiên tốt nghiệp lớp hội hoạ các trường ĐH mỹ thuật.<br />
+                    – Có khả năng làm việc độc lập và nhóm tốt.<br />
+                </Typography>
+                <Typography
+                     variant="h2"
+                     color="#777"
+                     fontSize="16px"
+                     fontWeight="550"
+                     mt={2}
+                >
+                    Quyền lợi được hưởng
+                </Typography>
+                <Typography
+                     variant="subtitle"
+                     color="#777"
+                     fontSize="14px"
+                     
+                >
+                    – Làm việc trong môi trường sáng tạo và thân thiện.<br />
+                    – Được làm việc với các nhóm vẽ chuyên nghiệp, có nhiều năm kinh nghiệm.<br />
+                    – Cơ hội phát triển bản thân cũng như nghề nghiệp, có thể gắn bó
+                    lâu dài với thu nhập hấp dẫn.
+                </Typography>
+                <Typography
+                     variant="h2"
+                     color="#777"
+                     fontSize="16px"
+                     fontWeight="550"
+                     mt={2}
+                >
+                    Liên hệ
+                </Typography>
+                <Typography
+                     variant="subtitle"
+                     color="#777"
+                     fontSize="14px"
+                >
+                    Cửa hàng Khung & Tranh<br />
+                    Địa chỉ: 385 đường Ngọc Hồi, Thanh Trì, Hà Nội<br />
+                    Số đt: 0983568361
+                </Typography>
+                <Typography
+                     variant="h2"
+                     color="#777"
+                     fontSize="16px"
+                     fontWeight="550"
+                     mt={2}
+                >
+                    Loại hình công việc
+                </Typography>
+                <Typography
+                     variant="subtitle"
+                     color="#777"
+                     fontSize="14px"
+                >
+                    – Toàn thời gian, Bán thời gian<br />
+                    – Lương: Thỏa thuận
+                </Typography>
+            </Box>
+        </BoxBody>
+    );
+}
