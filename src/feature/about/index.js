a5828f22b5eaa3b3136d5896189b7ec7d7f9c773
TungNguyen0510 <88915666+TungNguyen0510@users.noreply.github.com>
2023-06-18 13:42:28 +0700

    add about

---
+++ src/feature/about/index.js
@@ -0,0 +1,148 @@
+import BoxBody from "../../components/box-body";
+import {
+    Box,
+    Typography,
+} from "@mui/material";
+export default function About() {
+    return (
+        <BoxBody>
+            <Box color="#777">
+                <Typography
+                    variant="h1"
+                    color="#555"
+                    fontSize="22px"
+                    fontWeight="550"
+                    mb={2}
+                >
+                    Về Bán Tranh – Tranh Phong, Cảnh,
+                    Tranh Sơn Dầu, Tranh Nghệ Thuật & Khung Tranh Đẹp
+                </Typography>
+                <Box sx={{
+                    display: "flex",
+                    justifyContent: "between",
+                }}>
+                    <Typography
+                        variant="subtitle1" mt={2} mr={6} textAlign="justify"
+                    >
+                        Kính chào quý khách, lời đầu tiên chúng tôi xin chân
+                        thành cảm ơn quý khách đã tín nhiệm và ủng chúng tôi
+                        trong suốt thời gian chúng tôi xây dựng và phát triển
+                        cửa hàng Khung & Tranh Mộc Sinh. Trong những năm gần
+                        đây nhu cầu sử dụng tranh để trang trí cho các phòng
+                        trong gia đình, nhà hàng, khách sạn, thậm chí là những
+                        bức tranh tường ở các nơi khu dân cư ngày càng tăng và
+                        được ưa chuộng. Đó là một nhu cầu làm đẹp tất yếu cho
+                        cuộc sống và đáp ứng lại nhu cầu này, chúng tôi đã đã
+                        luôn luôn cải tiến dịch vụ để phục vụ quý khách tốt nhất.
+                        Chính vì lẽ đó, chúng tôi cùng các họa sĩ tài năng trên
+                        khắp Việt Nam đã cho ra đời những bức tranh sơn dầu đẹp,
+                        những khung tranh ấn tượng nhất để phục vụ mọi người trên toàn quốc.
+                        Cùng với sự làm việc không biết mệt mỏi của đội ngũ nhân viên
+                        Bán Tranh, những họa sĩ tài hoa, đầy sáng tạo, cùng các chuyên
+                        gia hội họa dày dạn kinh nghiệm đã cho ra đời nhiều tác phẩm
+                        tranh độc đáo và ấn tượng. Các bức tranh phong cảnh như một
+                        sự quảng bá những hình ảnh đẹp nhất từ khắp mọi miền tổ quốc
+                        và các phong cảnh đẹp trên thế giới đến mọi người và khơi dậy
+                        tình yêu cái đẹp của con người Việt Nam. Cũng vậy, những bức
+                        tranh nghệ thuật, tranh sơn dầu…như càng làm tăng thêm vẻ đẹp,
+                        tô điểm thêm những sắc màu cuộc sông cho không gian của bạn
+                        thêm phần lộng lẫy và sang trọng.
+                    </Typography>
+                    <Box>
+                        <img src="https://bantranh.com/wp-content/uploads/2019/10/cua-hang-khung-tranh-385-duong-ngoc-hoi.jpg"
+                            alt="" width="423px" height="527px" />
+                        <Typography 
+                        variant="subtitle1"
+                        textAlign="center"
+                        color="#993300"
+                        fontSize="14px"
+                        fontWeight="540"
+                        fontStyle="italic"
+                        >
+                            Cửa hàng Khung & Tranh Mộc Sinh – 385 đường Ngọc Hồi, Thanh Trì, Hà Nội – Điện Thoại: 0961136825
+                        </Typography>
+                    </Box>
+
+                </Box>
+                <Typography
+                    variant="h1"
+                    color="#555"
+                    fontSize="18px"
+                    fontWeight="550"
+                    mb={2}
+                >
+                    Nhiệm vụ của chúng tôi
+                </Typography>
+                <Typography>
+                    – Chúng tôi luôn luôn cố gắng cải thiện kỹ năng mà chuyên môn
+                    để tạo ra những tác phẩm đẹp và phục vụ quý khách với chất lượng tốt nhất.<br />
+                    – Những yêu cầu của khách như những thước đo để chúng tôi phấn
+                    đấu. Những đánh giá của quý khách như những lời khuyên giúp chúng tôi cải thiện để ngày một tốt hơn.<br />
+                    – Chúng tôi luôn luôn cố gắng lắng nghe và thấu hiểu mọi yêu cầu
+                    của quý khách để mang lại sự hài lòng cho quý khách. Đó cũng là
+                    kim chỉ nam cho chúng tôi để cải thiện dịch vụ ngày một tốt hơn.
+                </Typography>
+                <Typography
+                    variant="h1"
+                    color="#555"
+                    fontSize="18px"
+                    fontWeight="550"
+                    mb={2}
+                    mt={2}
+                >
+                    Đội ngũ sáng tác của chúng tôi
+                </Typography>
+                <Typography>
+                    Chúng tôi luôn luôn xây dựng một đội ngũ các tư vấn viên,
+                    kiến trúc sư, họa sĩ với sự nhiệt tình cao nhất, chăm chỉ và
+                    sáng tạo trong công việc, tạo ra những sản phẩm ưng ý nhất và
+                    luôn luôn đảm bảo chất lượng dịch vụ như yêu cầu.
+                </Typography>
+                <Typography
+                    variant="h1"
+                    color="#555"
+                    fontSize="18px"
+                    fontWeight="550"
+                    mb={2}
+                    mt={2}
+                >
+                    Những mục tiêu
+                </Typography>
+                <Typography>
+                    – Luôn luôn phục vụ chăm sóc khách hàng với tinh thần nhiệt tình và chu đáo nhất.<br />
+                    – Luôn cải thiện kỹ năng cho đội ngũ nhân viên phù hợp với những
+                    yêu cầu của xu hướng hiện tại ai ở Việt Nam hay thế giới.
+                </Typography>
+                <Typography
+                    variant="h1"
+                    color="#555"
+                    fontSize="18px"
+                    fontWeight="550"
+                    mb={2}
+                    mt={2}
+                >
+                    Thông tin liên hệ
+                </Typography>
+                <Typography
+                    variant="h1"
+                    color="#777"
+                    fontSize="14px"
+                    fontWeight="550"
+                    mb={1}
+                >
+                    Cửa hàng Khung & Tranh
+                </Typography>
+                <Typography>
+
+                    Địa chỉ: 385 đường Ngọc Hồi, Thanh Trì, Hà Nội<br />
+                    Email: info@bantranh.com <br />
+                    Điện thoại: 0961136825<br />
+                    Phone: 0983568361<br />
+                    <br />
+                    Chúng tôi xin chân thành cảm ơn quý khách đã ủng hộ Bán Tranh.<br />
+                    Chúng tôi xin chúc quý khách an khang thịnh vượng!
+                </Typography>
+            </Box>
+        </BoxBody>
+    );
+};
