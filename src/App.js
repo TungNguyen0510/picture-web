c449434263b809f13739e2af556df0ae25a2b90b
TungNguyen0510 <88915666+TungNguyen0510@users.noreply.github.com>
2023-06-18 12:56:15 +0700

    add app.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { APP_ROUTER } from "./router";
+import NotFoundPage from "./feature/not-found";
+import PrivateLayout from "./layout/private";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        {APP_ROUTER.map((route) => (
+          <Route
+            key={route.path}
+            path={route.path}
+            element={
+              <PrivateLayout>
+                <route.element />
+              </PrivateLayout>
+            }
+          />
+        ))}
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
